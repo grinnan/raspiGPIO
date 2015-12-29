3d34cb377b9e2eaa0b17acb0b84b831299d0952d
grinnan <grinnan>
2015-12-29 13:42:23 -0600

    add gpioMASTER.py

---
+++ gpioMASTER.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import logging
+import RPi.GPIO as GPIO
+import time
+import datetime
+from poolcon import logdat
+
+# GPIO PIN NUMBERS, INPUT: BUTTON, OUTPUT: LED
+inpin = 14
+outpin = 15
+count = 0
+sixty = 0
+countdown = 1
+gpioout=1
+
+#INITIALIZE GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(inpin,GPIO.IN)
+GPIO.setup(outpin,GPIO.OUT)
+
+#LOOP
+logging.info('BEGIN LOOP')
+
+while True:
+    input = GPIO.input(inpin)
+    sixty += 1
+    if (sixty == 10):
+        sixty=0
+        logdat('SIXTY',gpioout,count,countdown)
+
+    if input > 0:
+        count = count + 1
+        logdat('ACTIVATED',gpioout,count,countdown)
+        countdown = 30   
+    else:
+        countdown = countdown - 1
+        count = 0
+        logdat('OFF',gpioout,count,countdown)
+            
+    if count > 30:
+        GPIO.output(outpin,GPIO.HIGH)
+        if (gpioout != 1):  # If it is not equal to what its gonna be
+            logdat('CHANGE(LOW TO HIGH)',gpioout,count,countdown)
+            gpioout = 1
+            # countdown = 30
+
+    if countdown < 1:
+        GPIO.output(outpin,GPIO.LOW)
+        if (gpioout != -1):  # If it is not equal to what its gonna be
+            logdat('CHANGE(HIGH TO LOW)',gpioout,count,countdown)
+            gpioout = -1
+            
+    time.sleep(1.0)
+
